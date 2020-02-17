005cb875b96ff503f3b251d27efcede181f75841
Carlos Ramirez <calbertora@gmail.com>
2020-02-16 20:23:09 -0500

    Add the SignalR code to the ts file

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRWebPack.Hubs;
 
 namespace SignalRWebPack
 {
@@ -16,6 +17,7 @@ namespace SignalRWebPack
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +29,12 @@ namespace SignalRWebPack
             }
 
             app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<ChatHub>("/hub");
             });
         }
     }
